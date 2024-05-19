ff1ac572c3246c156af850e456d99dcf8129fb54
HiroLiang <hiromichi.liang@gmail.com>
2024-05-20 01:38:11 +0800

    #Start - Project - Villager A

---
+++ src/test/java/hiro/liang/games/villager_a/VillagerAApplicationTests.java
@@ -0,0 +1,13 @@
+package hiro.liang.games.villager_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillagerAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
